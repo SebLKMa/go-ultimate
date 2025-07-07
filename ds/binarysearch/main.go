3a447a69cabbdba7023c87b8a1ca34804275faf8
seblkma <sebmalikkeung@gmail.com>
2025-07-07 07:39:29 +0000

    simple binary search

---
+++ ds/binarysearch/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func bsearch(sortedArray []int, toSearch int) bool {
+	found := false
+	low := 0
+	high := len(sortedArray) - 1
+	for low <= high {
+		mid := (high + low) / 2
+		if toSearch == sortedArray[mid] {
+			found = true
+			break
+		}
+		if toSearch > sortedArray[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return found
+}
+
+func main() {
+	array := []int{1, 3, 4, 5}
+	to_search1 := 3
+	if !bsearch(array, to_search1) {
+		fmt.Println("expected true, got false")
+	} else {
+		fmt.Println("ok")
+	}
+
+	to_search2 := 6
+	if bsearch(array, to_search2) {
+		fmt.Println("expected false, got true")
+	} else {
+		fmt.Println("ok")
+	}
+}
