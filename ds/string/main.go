78b3faddf5d0ece60df2b4ff8d3c421062448a46
seblkma <sebmalikkeung@gmail.com>
2025-07-16 08:48:01 +0000

    added string removeDups :

--- ds/string/main.go
+++ ds/string/main.go
@@ -27,10 +27,51 @@ func reverse(src string) (dst string) {
 	return string(bsrc)
 }
 
+func removeDups(src string) string {
+	// Using rune instead of byte for unicode string
+	rsrc := []rune(src)
+	max := len(rsrc)
+
+	stopper := 1 // stopper starts at second position
+	for currentIndex := 0; currentIndex < max; currentIndex++ {
+		var runnerIndex int // runner always compares from beginning
+		for runnerIndex = 0; runnerIndex < stopper; runnerIndex++ {
+			if rsrc[runnerIndex] == rsrc[currentIndex] {
+				break // duplicate found
+			}
+		}
+
+		if runnerIndex == stopper {
+			// runner ran until stop point means no duplicate
+			rsrc[stopper] = rsrc[currentIndex]
+			stopper++
+		}
+	}
+
+	// If duplicates have been removed, remove the remaining "residue"
+	/*
+		fmt.Printf("%d\n", stopper)
+		for runnerIndex := stopper; runnerIndex < max; runnerIndex++ {
+			(*rsrc)[runnerIndex] = 0
+		}
+		fmt.Printf("%d\n", len(*rsrc))
+	*/
+	if stopper != max {
+		return string(rsrc)[:stopper]
+	}
+
+	return string(rsrc)
+}
+
 func main() {
 	//str := "123456"
 	str := "ahola"
 	fmt.Println(str)
 	str2 := reverse(str)
 	fmt.Println(str2)
+
+	str3 := "1223345566"
+	fmt.Printf("%s len:%d\n", str3, len(str3))
+	str4 := removeDups(str3)
+	fmt.Printf("%s len:%d\n", str4, len(str4))
 }
