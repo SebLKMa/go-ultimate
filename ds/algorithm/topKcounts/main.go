f1a9760003c8c169f08228a264a49ceb5a6c09df
seblkma <sebmalikkeung@gmail.com>
2025-07-30 01:57:51 +0000

    top k counts using sort custom struct

--- ds/algorithm/topKcounts/main.go
+++ ds/algorithm/topKcounts/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"slices"
+	"sort"
 )
 
 func topKcounts(input []int, k int) (output []int) {
@@ -14,28 +15,41 @@ func topKcounts(input []int, k int) (output []int) {
 		return
 	}
 
+	type kcountStruct = struct {
+		kcount int
+		key    int
+	}
+	kcounts := []kcountStruct{}
+
 	slices.Sort(input)
-	max := len(input) - 1
+	maxLen := len(input) - 1
 	count := 1
-	counts := []int{}
-	indices := []int{}
 	index := 0
-	for index = 1; index <= max; index++ {
+	for index = 1; index <= maxLen; index++ {
 		if input[index] != input[index-1] {
-			counts = append(counts, count)
-			indices = append(indices, index)
+			kcounts = append(kcounts, kcountStruct{count, input[index-1]})
 			count = 0
 		}
 		count++
 	}
+	// Remaining count of the last element
 	if count != 0 {
-		counts = append(counts, count)
-		indices = append(indices, index)
+		kcounts = append(kcounts, kcountStruct{count, input[index-1]})
 	}
 
-	fmt.Printf("counts  %v\n", counts)
-	fmt.Printf("indices  %v\n", indices)
+	// Sort descending
+	sort.Slice(kcounts, func(i, j int) bool {
+		return kcounts[i].kcount > kcounts[j].kcount
+	})
+	fmt.Printf("kcounts  %v\n", kcounts)
 
+	// Return the top Ks
+	for i, kc := range kcounts {
+		if i >= k {
+			break
+		}
+		output = append(output, kc.key)
+	}
 	return
 }
 
