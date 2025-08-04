a64caec0734d59ae83083ee0f9f62fc7f2e3197f
seblkma <sebmalikkeung@gmail.com>
2025-08-04 07:29:17 +0000

    correct logic

--- ds/algorithm/arraymaxarea/arraymaxarea.go
+++ ds/algorithm/arraymaxarea/arraymaxarea.go
@@ -15,7 +15,7 @@ func minInt(x int, y int) int {
 }
 
 // Returns the max area from the array of heights
-func maxArea(heights []int) int {
+func maxAreaWrong(heights []int) int {
 	count := len(heights)
 	if count == 0 {
 		return 0
@@ -31,8 +31,33 @@ func maxArea(heights []int) int {
 			maxArea = area
 		}
 		j -= i
-		if j == i || j < 0 {
+		if j <= 0 {
 			break
+		}
+	}
+	return maxArea
+}
+
+// https://leetcode.com/problems/container-with-most-water/solutions/7031738/best-explanation-visualization-simple-fast-clear/
+func maxArea(heights []int) int {
+	left := 0                 // start of the array on X-axis
+	right := len(heights) - 1 // end of the array on X-axis
+	maxArea := 0
+	for left < right {
+		// 1. Current_area = min(height[left], height[right]) × (right - left).
+		height := minInt(heights[left], heights[right]) // heights on Y-axis
+		width := right - left
+		area := height * width
+		fmt.Println(height, width, area)
+		// 2. Update {max_area}.
+		if area > maxArea {
+			maxArea = area
+		}
+		// 3. Move {left} pointer inwards if height[left] < height[right]; else move {right} pointer inwards.
+		if heights[left] < heights[right] {
+			left++
+		} else {
+			right--
 		}
 	}
 	return maxArea
